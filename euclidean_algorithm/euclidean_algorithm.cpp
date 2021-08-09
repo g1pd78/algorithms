240c1b3b526b60df6d18fe6c869081bdc6e85c91
g1pd78 <g1pd78@gmail.com>
2021-08-09 20:48:43 +0500

    euclidean_algorithm

---
+++ euclidean_algorithm/euclidean_algorithm.cpp
@@ -0,0 +1,20 @@
+﻿#include <cassert>
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+template <class Int>
+Int gcd(Int a, Int b) {
+    assert(a >= 0 && b >= 0);
+    return b ? gcd(b, a % b) : a;
+}
+
+
+
+int main(void) {
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl;
+    return 0;
+}
