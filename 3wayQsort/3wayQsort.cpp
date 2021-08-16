02c20dce3f98658ab31db080a97f76ecf9954274
g1pd78 <g1pd78@gmail.com>
2021-08-16 15:35:28 +0500

    3way qsort

--- 3wayQsort/3wayQsort.cpp
+++ 3wayQsort/3wayQsort.cpp
@@ -14,13 +14,43 @@ void printArray(int* arr, int n) {
 	cout << '\n';
 }
 
+void partition(int *arr, int leftPointer, int rightPointer, int &i, int &j) {
+	if (rightPointer - leftPointer == 1) {
+		if(arr[leftPointer] > arr[rightPointer])
+			swap(arr[leftPointer], arr[rightPointer]);
+		i = leftPointer;
+		j = rightPointer;
+		return;
+	}
+
+	int pivot = arr[rightPointer];
+	int middlePointer = leftPointer;
+
+	while (middlePointer <= rightPointer) {
+		if (arr[middlePointer] < pivot)
+			swap(arr[leftPointer++], arr[middlePointer++]);
+		else if (arr[middlePointer] == pivot)
+			middlePointer++;
+		else if (arr[middlePointer] > pivot)
+			swap(arr[middlePointer], arr[rightPointer--]);
+	}
+
+	i = leftPointer - 1;
+	j = middlePointer;
+}
+
 void qsort(int* arr, int leftSide, int rightSide) {
+	if (leftSide <= rightSide) {
+		int i, j;
+		partition(arr, leftSide, rightSide, i, j);
+		qsort(arr, leftSide, i);
+		qsort(arr, j, rightSide);
+	}
 }
 
 int main()
 {
 	int n, inputArray[100000];
-	size_t size = sizeof(inputArray) / sizeof(int);
 
 	cin >> n;
 	for (int i = 0; i < n; i++)
