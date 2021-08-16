9d4199ca71975ffd50295016c711e0c60118e419
g1pd78 <g1pd78@gmail.com>
2021-08-16 14:41:16 +0500

    2way qsort

--- points/qsort.cpp
+++ points/qsort.cpp
@@ -2,11 +2,45 @@
 
 using namespace std;
 
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void printArray(int* arr, int n) {
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << '\t';
+	cout << '\n';
+}
+
+int partition(int *arr, int leftPointer, int rightPointer) {
+	int iter = leftPointer - 1;
+	int pivot = arr[rightPointer];
+	for (int i = leftPointer; i < rightPointer;i++) {
+		if (arr[i] < pivot) {
+			swap(arr[++iter], arr[i]);
+		}
+	}
+	swap(arr[++iter], arr[rightPointer]);
+	return iter;
+}
+
+void qsort(int *arr, int leftSide, int rightSide) {
+	if (leftSide < rightSide) {
+		int pivot = partition(arr, leftSide, rightSide);
+		qsort(arr, leftSide, pivot - 1);
+		qsort(arr, pivot + 1, rightSide);
+	}
+}
+
 int main()
 {
-	int n, m;
+	int n, inputArray[100000];
 
 	cin >> n;
-	for(int i = 0;i < n;i++)
-
+	for (int i = 0; i < n; i++)
+		cin >> inputArray[i];
+	qsort(inputArray, 0, n - 1);
+	printArray(inputArray, n);
 }
