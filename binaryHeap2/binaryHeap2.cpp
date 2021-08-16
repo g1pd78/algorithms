bc4c1996ebc4949d351472a06b4cfdbe7ac33ee0
g1pd78 <g1pd78@gmail.com>
2021-08-16 19:15:54 +0500

    binaryHeap on array

---
+++ binaryHeap2/binaryHeap2.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+class MaxHeap{
+private:
+	int *arr;
+	int size;
+	int capacity;
+public:
+	MaxHeap(int _capacity);
+	~MaxHeap() {
+		delete[] arr;
+	}
+	
+	void MaxHeapify(int );
+
+	int parent(int i) { return (i - 1) / 2; }
+
+	int leftChild(int i) { return (2 * i + 1); }
+
+	int rightChild(int i) { return (2 * i + 2); }
+
+	int getMax();
+
+	void addPoint(int x);
+
+	void print() {
+		for (int i = 0; i < size; i++)
+			cout << arr[i] << '\t';
+		cout << '\n';;
+	}
+
+};
+
+MaxHeap::MaxHeap(int _capacity) {
+	capacity = _capacity;
+	size = 0;
+	arr = new int[_capacity];
+}
+
+void MaxHeap::addPoint(int x) {
+	arr[size] = x;
+	int i = size;
+	while (arr[parent(i)] < arr[i] && i > 0) {
+		swap(arr[parent(i)], arr[i]);
+		i = parent(i);
+	}
+	size++;
+}
+
+int MaxHeap::getMax() {
+	if (size == 0) {
+		return -1;
+	}//////1 
+	int heapRoot = arr[0];
+	arr[0] = arr[size - 1];
+	size--;
+	MaxHeapify(0);
+	return heapRoot;
+}
+
+void MaxHeap::MaxHeapify(int i) {
+	int left = leftChild(i);
+	int right = rightChild(i);
+	int largestChild = i;
+
+	if (left < size && arr[left] > arr[largestChild])
+		largestChild = left;
+	if (right < size && arr[right] > arr[largestChild])
+		largestChild = right;
+	if (largestChild != i) {
+		swap(arr[largestChild], arr[i]);
+		MaxHeapify(largestChild);
+	}
+}
+
+
+int main()
+{
+	MaxHeap task(100000);
+	int n, val;
+	string com;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> com;
+		if (com == "Insert") {
+			cin >> val;
+			task.addPoint(val);
+		}
+		else cout << task.getMax() << '\n';
+		///task.print();
+	}
+	return 0;
+}
